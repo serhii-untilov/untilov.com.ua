74c56d07ddfec9cccbfa618678290c2919b8d601
Sergey Untilov <s.untilov@gmail.com>
2024-04-19 08:59:14 +0300

    Smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="scroll-smooth focus:scroll-auto max-md:scroll-auto">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
