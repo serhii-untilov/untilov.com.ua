0cc4e4c041739757bb9dad3b7c59e057be96dbf0
Sergey Untilov <s.untilov@gmail.com>
2024-04-19 21:05:32 +0300

    Minor update

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -24,7 +24,7 @@ export function ExperienceItem(props: any) {
             >
                 <div className="w-full flex flex-col justify-between gap-1 m-4 p-6 rounded-md border-2 border-white shadow-md bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40">
                     <div className="flex flex-col justify-start">
-                        <div className="mx-auto bg-white rounded-full m-2 w-24 h-24 flex justify-center border-2 border-gray-200">
+                        <div className="mx-auto bg-white rounded-full m-0 w-24 h-24 flex justify-center border-2 border-gray-200">
                             <Image
                                 src={experienceItem.image}
                                 alt="Experience Image"
