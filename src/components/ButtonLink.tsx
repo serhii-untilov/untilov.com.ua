10f7644e154c4405c290269999a9e824f9b2de95
Sergey Untilov <s.untilov@gmail.com>
2024-05-08 18:08:54 +0300

    Demo button fixed

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -17,6 +17,7 @@ export function ButtonLink(props: Props) {
     return (
         <a
             href={href}
+            target="_blank"
             className="bg-blue-50 text-blue-500 w-full border-2 border-solid max-w-48 font-medium rounded-lg text-center text-sm px-5 py-2 border-blue-200 hover:bg-blue-100 shadow-sm"
         >
             {children}
