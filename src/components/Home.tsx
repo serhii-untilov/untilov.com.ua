41a4e512a275033d20bad1cf3624b9bb00c56f6e
Sergey Untilov <s.untilov@gmail.com>
2024-05-08 13:57:37 +0300

    Hero image size

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,10 +5,10 @@ import Tooltip from './Tooltip';
 
 export function Home(cv: any) {
     return (
-        <div className="flex flex-col h-full justify-center max-md:justify-normal max-md:mx-4">
-            <div className="flex flex-row justify-between gap-8 max-md:gap-0 max-md:flex-col-reverse max-md:justify-start">
-                <div className="flex flex-col justify-around basis-2/3 max-md:basis-full">
-                    <h2 className="text-5xl font-extrabold text-gray-800 max-md:text-center mb-4">
+        <div className="flex flex-col h-full justify-center max-sm:justify-normal max-sm:mx-4">
+            <div className="flex flex-row justify-between gap-8 max-sm:gap-0 max-sm:flex-col-reverse max-sm:justify-start">
+                <div className="flex flex-col justify-around basis-2/3 max-sm:basis-full">
+                    <h2 className="text-5xl font-extrabold text-gray-800 max-sm:text-center mb-4">
                         {cv.job}
                     </h2>
 
@@ -112,19 +112,19 @@ export function Home(cv: any) {
                             </li>
                         ) : null}
 
-                        <li className="pl-12 max-md:pl-4">
+                        <li className="pl-12 max-sm:pl-4">
                             <Status cv={cv} />
                         </li>
                     </ul>
                 </div>
 
-                <a href="#about" className="basis-1/3 max-md:basis-full my-4 mx-auto">
+                <a href="#about" className="basis-1/3 max-sm:basis-full my-4 mx-auto">
                     <Image
                         src="/files/hero.png"
                         alt={'Hero image'}
                         className="animate-morph border-4 border-gray-700 brightness-125 shadow-2xl"
-                        width={300}
-                        height={300}
+                        width={280}
+                        height={280}
                         priority
                         quality={100}
                     />
@@ -139,8 +139,8 @@ export function Home(cv: any) {
                         'pr-4',
                         'mr-2',
                         'text-center',
-                        'max-md:pr-0',
-                        'max-md:mx-auto',
+                        'max-sm:pr-0',
+                        'max-sm:mx-auto',
                         'max-lg:mb-5',
                     ].join(' ')}
                 >
