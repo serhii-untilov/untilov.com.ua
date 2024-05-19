759faacc134571641a9d41b6da138d9dc0823bd6
Sergey Untilov <s.untilov@gmail.com>
2024-05-19 09:53:27 +0300

    Color for status component

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -13,7 +13,8 @@ export function Status(props: any) {
 
     return (
         <a href="#contact">
-            <div className="-rotate-12 rounded border-dashed text-center border-2 border-indigo-600 font-bold text-indigo-600 w-fit p-4 uppercase hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:border-solid hover:border-white transition-all">
+            {/* <div className="-rotate-12 rounded border-dashed text-center border-2 border-indigo-600 font-bold text-indigo-600 w-fit p-4 uppercase hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:border-solid hover:border-white transition-all"> */}
+            <div className="-rotate-12 rounded border-dashed text-center border-2 border-blue-500 font-bold text-blue-500 w-fit p-4 uppercase hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:border-solid hover:border-white transition-all">
                 {generateStatusList()}
             </div>
         </a>
