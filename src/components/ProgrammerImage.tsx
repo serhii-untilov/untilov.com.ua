0e5de3145b739cbdbc032c80c318f3ca13c469b2
Sergey Untilov <s.untilov@gmail.com>
2024-08-04 23:33:42 +0300

    fix: temporary remove broken programmer image component

--- src/components/ProgrammerImage.tsx
+++ src/components/ProgrammerImage.tsx
@@ -2,21 +2,29 @@ import Image from 'next/image';
 
 const ProgrammerImage = () => {
     return (
-        <div className="relative w-full h-full">
-            <Image
-                src="/images/ring-text.svg"
-                alt={'Ring text image'}
-                className="absolute left-0 top-0 rounded-full h-40 w-40 animate-spinSlow m-auto p-2"
-                width={160}
-                height={160}
-            />
-            <Image
-                src="/images/programmer.png"
-                alt={'Programmer image'}
-                className="m-auto p-10"
-                width={160}
-                height={160}
-            />
+        <div
+            className={[
+                'absolute left-72 top-52 rounded-full bg-white h-40 w-40',
+                'max-sm:sr-only max-lg:left-96 border-2 border-solid border-white',
+                'hover:scale-105 hover:border-blue-200 transition-all',
+            ].join('')}
+        >
+            <div className="relative w-full h-full">
+                <Image
+                    src="/images/ring-text.svg"
+                    alt={'Ring text image'}
+                    className="absolute left-0 top-0 rounded-full h-40 w-40 animate-spinSlow m-auto p-2"
+                    width={160}
+                    height={160}
+                />
+                <Image
+                    src="/images/programmer.png"
+                    alt={'Programmer image'}
+                    className="m-auto p-10"
+                    width={160}
+                    height={160}
+                />
+            </div>
         </div>
     );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,15 +16,7 @@ const About = ({ cv }) => {
                     <div className="basis-1/2 max-md:basis-full max-md:sr-only">
                         <a href="#projects" className="relative">
                             <DeskImage />
-                            <div
-                                className={[
-                                    'absolute left-72 top-52 rounded-full bg-white h-40 w-40',
-                                    'max-sm:sr-only max-lg:left-96 border-2 border-solid border-white',
-                                    'hover:scale-105 hover:border-blue-200 transition-all',
-                                ].join('')}
-                            >
-                                <ProgrammerImage />
-                            </div>
+                            {/* <ProgrammerImage /> */}
                         </a>
                     </div>
 
