5849419bc4353e5801198a516449f81a5c0a4fac
Sergey Untilov <s.untilov@gmail.com>
2024-08-04 09:15:24 +0300

    fix: status button

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -12,7 +12,7 @@ export function Status(props: any) {
     };
 
     return (
-        <a href={cv.cv ?? '#contact'}>
+        <a href="#contact">
             <div className="-rotate-12 rounded border-dashed text-center border-2 border-blue-500 font-bold text-blue-500 w-fit p-4 uppercase hover:text-white hover:bg-gradient-to-r hover:from-blue-500 hover:via-purple-500 hover:to-pink-500 hover:border-solid hover:border-white transition-all">
                 {generateStatusList()}
             </div>
