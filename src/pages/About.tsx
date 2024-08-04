ed94ad9dde9febd0be2a36a54174eee641537daf
Sergey Untilov <s.untilov@gmail.com>
2024-08-05 00:09:27 +0300

    fix: programmer image component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,10 +6,6 @@ import RotatedRingImage from '@/components/RotatedRingImage';
 import { useMemo } from 'react';
 
 const About = ({ cv }) => {
-    const description = cv?.about?.description?.map((element: string, index: number) => {
-        return <p key={index}>{element}</p>;
-    });
-
     return (
         <>
             {cv && (
@@ -22,7 +18,7 @@ const About = ({ cv }) => {
                     <div className="basis-1/2 max-md:basis-full max-md:sr-only">
                         <a href="#projects" className="relative">
                             <DeskImage />
-                            {/* <div
+                            <div
                                 className={[
                                     'absolute left-72 top-52 rounded-full bg-white h-40 w-40',
                                     'max-sm:sr-only max-lg:left-96 border-2 border-solid border-white',
@@ -33,14 +29,16 @@ const About = ({ cv }) => {
                                     <RotatedRingImage />
                                     <ProgrammerImage />
                                 </div>
-                            </div> */}
+                            </div>
                         </a>
                     </div>
 
                     <div className="flex flex-col gap-6 basis-1/2 max-md:basis-full">
                         <PageTitle>About Me</PageTitle>
                         {cv.about?.title && <PageSubTitle>{cv.about?.title}</PageSubTitle>}
-                        {description}
+                        {cv?.about?.description?.map((element: string, index: number) => {
+                            return <p key={index}>{element}</p>;
+                        })}
                     </div>
                 </div>
             )}
