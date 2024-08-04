ed4f79cd0e0072d86d32017b1cdd73eab286c4d0
Sergey Untilov <s.untilov@gmail.com>
2024-08-04 23:53:51 +0300

    fix: temporary remove broken programmer image component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -17,7 +17,7 @@ const About = ({ cv }) => {
                     <div className="basis-1/2 max-md:basis-full max-md:sr-only">
                         <a href="#projects" className="relative">
                             <DeskImage />
-                            <div
+                            {/* <div
                                 className={[
                                     'absolute left-72 top-52 rounded-full bg-white h-40 w-40',
                                     'max-sm:sr-only max-lg:left-96 border-2 border-solid border-white',
@@ -28,7 +28,7 @@ const About = ({ cv }) => {
                                     <RotatedRingImage />
                                     <ProgrammerImage />
                                 </div>
-                            </div>
+                            </div> */}
                         </a>
                     </div>
 
