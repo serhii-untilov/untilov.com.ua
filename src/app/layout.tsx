6a4482a463b38ed267bcd4a34474c578931a349f
Serhii Untilov <s.untilov@gmail.com>
2024-09-11 11:14:43 +0300

    chore: freelancehunt meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout(props: PropsWithChildren) {
         <html lang="en" className="scroll-smooth focus:scroll-auto max-md:scroll-auto">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <meta name="freelancehunt" content="19070ee94ecf3d4" />
             </head>
             <body className={inter.className}>{props.children}</body>
         </html>
